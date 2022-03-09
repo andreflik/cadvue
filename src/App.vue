6a95ebe07e63b59ac0c07d1bfce116d4958d5009
Andre Fagundes <andre.fagundes@fmxsolucoes.com.br>
2022-03-09 10:54:02 -0300

    Add interceptores no axios

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<div id="app" class="container">
+		<h1>Cadastro de Contatos</h1>
 		<b-card>
 			<b-form-group label="Nome:">
 				<b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe um nome"></b-form-input>
@@ -8,14 +9,26 @@
 				<b-form-input type="email" size="lg" v-model="usuario.email" placeholder="Informe um email"></b-form-input>
 			</b-form-group>
 			<b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
+			<b-button @click.prevent="obterUsuario" size="lg" variant="success" class="ml-2">Lista de Contatos</b-button>
 		</b-card>
+		<hr>
+		<b-list-group>
+			<b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
+				<strong>Nome: </strong> {{usuario.nome}} <br>
+				<strong>E-mail: </strong> {{usuario.email}} <br>
+				<strong>ID: </strong> {{usuario.id}}
+			</b-list-group-item>
+		</b-list-group>
 	</div>
 </template>
 
 <script>
+// import axios from 'axios'
 export default {
+	
 	data() {
 		return {
+			usuarios: [],
 			usuario: {
 				nome: '',
 				email: ''
@@ -24,8 +37,27 @@ export default {
 	},
 	methods: {
 		salvar() {
-			console.log(this.usuario)
-		}
+			// Axios Local - POST
+			// axios('https://cadvue-default-rtdb.firebaseio.com/usuarios.json', this.usuario)
+
+
+			this.$http.post('usuarios.json', this.usuario)
+				.then( () => {
+					this.usuario.nome = ''
+					this.usuario.email = ''
+				})
+		},
+		
+		obterUsuario() {
+			this.$http.get('usuarios.json').then(res => {
+				this.usuarios = res.data
+
+				// Axios Local - GET
+			// axios('https://cadvue-default-rtdb.firebaseio.com/usuarios.json').then(res => {
+			// 	this.usuarios = res.data
+				
+			})
+			}
 	}
 	// created() {
 	// 	this.$http.post('usuarios.json', {
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -9,5 +9,22 @@ Vue.use({
         Vue.prototype.$http = axios.create({
             baseURL: 'https://cadvue-default-rtdb.firebaseio.com/'
         })
+
+
+        // Interceptors - method
+        Vue.prototype.$http.interceptors.request.use(config => {
+            // console.log(config.method)
+            return config
+        }, error => Promise.reject(error))
+
+
+        Vue.prototype.$http.interceptors.response.use(res => {
+            const array = []
+            for (let chave in res.data) {
+                array.push({id: chave, ...res.data[chave]})
+            }
+            res.data = array
+            return res
+        }, error => Promise.reject(error))
     }
 })
